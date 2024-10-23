7dbf4566dd8a099d8ec5a6829f9470ce73598272
Yana <annetart1994@gmail.com>
2024-10-23 15:31:52 +0300

    refactor: replace http server to another file

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import * as http from 'node:http';
+import { env } from 'node:process';
+import httpServer from './servers/http-server';
 
-export const httpServer = http.createServer(function (req, res) {
-  const __dirname = path.resolve(path.dirname(''));
-  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-  fs.readFile(file_path, function (err, data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-});
+const HTTP_PORT = Number(env.HTTP_PORT) || 8181;
 
-const HTTP_PORT = 8181;
-
-console.log(`Start static http server at http://localhost:${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+console.log(`Start static http server at http://localhost:${HTTP_PORT}`);
